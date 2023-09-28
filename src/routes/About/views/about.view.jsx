c5ed3be50dad86ea25be0f959a5b184c8ea93e48
Kim Anh Do <kim@Kims-MBP.fritz.box>
2023-09-28 15:25:04 +0200

    Updated

--- src/routes/About/views/about.view.jsx
+++ src/routes/About/views/about.view.jsx
@@ -1,5 +1,33 @@
 import "../about.style.css";
 
+import { HeaderButtonGroup } from "./../../../components/Buttons/HeaderButtonGroup/headerButtonGroup.component";
+import { BtnDownloadResume } from "./../../../components/Buttons/btnDownloadResume/btnDownloadResume.component";
+import { SkillsSlider } from "./../../../components/SkillsSlider/skillsSlider.component";
+
 export function AboutView() {
-  return <h1 className="about-title">About Me</h1>;
+  return (
+    <>
+      <HeaderButtonGroup />
+      <section className="about">
+        <h1 className="about-title">About me</h1>
+        <p className="about-text">
+          Hi, I am a web developer based in Munich, and I recently completed the
+          DCI Web Development Coding Course. Throughout the course, I&apos;ve
+          honed my skills in crafting elegant websites and web applications.{" "}
+          <br />
+          <br />
+          Passionate about writing clean, high-quality code, my focus is on
+          building solutions that are not only functional but also scalable and
+          easily maintainable.
+          <br />
+          <br />
+          I&apos;m comfortable working on my own, but collaborating with a team
+          also gives me great enjoyment. I&apos;m excited to connect with
+          companies and colleagues that inspire me!
+        </p>
+        <BtnDownloadResume />
+        <SkillsSlider />
+      </section>
+    </>
+  );
 }
